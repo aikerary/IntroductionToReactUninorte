43d95472580d4280b4e59ab0be5d5803561e3249
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-04 11:29:07 -0500

    Trying to do the same with functional component

--- src/components/MyClassComponent/MyClassComponent.jsx
+++ src/components/MyClassComponent/MyClassComponent.jsx
@@ -7,14 +7,22 @@ class MyClassComponent extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state={
+      amount: 0
+    };
   }
 
   render() {
     const { title, description } = this.props;
+    const {amount} = this.state;
     return (
       <>
         <h1>Hello from {title}</h1>
         <p>{description}</p>
+        <hr />
+        <p>My amount is {amount}</p>
+        <button onClick={() => this.setState({amount: amount + 1})}>Add</button>
+        <button onClick={() => this.setState({amount: amount - 1})}>Subtract</button>
       </>
     );
   }
--- src/components/MyFunctionalComponent/MyFunctionalComponent.jsx
+++ src/components/MyFunctionalComponent/MyFunctionalComponent.jsx
@@ -1,12 +1,19 @@
 // Functional components
 // Create a good practice functional components named myComponent
 import React from "react";
+import {useState} from "react";
 
 function MyFunctionalComponent({price, stock}) {
+    const [amount, setAmount] = useState(0);
   return (
     <>
       <h1>Hello from My Component {price}</h1>
-      <p>My component is a functional component {stock}</p>
+      <hr />
+      <p>My amount is {stock}</p>
+      <hr />
+        <p>My amount is {amount}</p>
+        <button onClick={() => setAmount(amount + 1)}>Add</button>
+        <button onClick={() => setAmount(amount - 1)}>Subtract</button>
     </>
   );
 }
