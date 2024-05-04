b8a037680d7364f1478e4b5a97facb0ac9334a4b
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-04 10:28:24 -0500

    Functional components and substracting the strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 //Class components
 // Create a good practice class components named myComponent
 
-class MyComponent extends React.Component {
+class MyClassComponent extends React.Component {
+  myMethod() {
+    console.log("Hello from myMethod");
+  }
   render() {
     return (
       <>
-      <h1>Hello from My Component</h1>
-      <p>My component is a class component</p>
+        <h1>Hello from My Component</h1>
+        <p>My component is a class component</p>
       </>
-    )
+    );
   }
 }
 
-//Functional components
+// Functional components
+// Create a good practice functional components named myComponent
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MyComponent />
-  </React.StrictMode>,
-)
+function MyFunctionalComponent() {
+  return (
+    <>
+      <h1>Hello from My Component</h1>
+      <p>My component is a functional component</p>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <MyClassComponent />
+    <MyFunctionalComponent />
+  </>
+);
