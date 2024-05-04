9c5f3eb0d9c880707854becd36727a65d3464746
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-04 09:46:42 -0500

    Class components

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//Class components
+// Create a good practice class components named myComponent
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <>
+      <h1>Hello from My Component</h1>
+      <p>My component is a class component</p>
+      </>
+    )
+  }
+}
+
+//Functional components
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <MyComponent />
   </React.StrictMode>,
 )
